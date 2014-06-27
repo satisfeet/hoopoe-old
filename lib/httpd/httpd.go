75c8d1b870de465e8e179f1b2602559c55427662
Bodo Kaiser <i@bodokaiser.io>
2014-06-27 10:33:07 +0200

    moved httpd middleware to /lib/httpd/mux/handles

--- lib/httpd/httpd.go
+++ lib/httpd/httpd.go
@@ -1,11 +1,7 @@
 package httpd
 
 import (
-    "log"
-    "errors"
-    "strings"
     "net/http"
-    "encoding/base64"
 
     "github.com/satisfeet/hoopoe/lib/httpd/mux"
 )
@@ -13,8 +9,8 @@ import (
 func Listen(c map[string]string) {
     m := mux.New()
 
-    m.Use(auth)
-    m.Use(logger)
+    m.Use(mux.Auth)
+    m.Use(mux.Logger)
 
     m.Get("/customers", CustomersList)
     m.Pos("/customers", CustomersCreate)
@@ -23,38 +19,4 @@ func Listen(c map[string]string) {
     m.Del("/customers/:customer", CustomersDestroy)
 
     http.ListenAndServe(c["port"], m)
-}
-
-func auth(c *mux.Context) {
-    h := strings.Split(c.Get("Authorization"), " ")
-
-    if len(h) != 2 {
-        c.RespondError(errors.New("Unauthorized"), 401)
-
-        return
-    }
-
-    b, err := base64.StdEncoding.DecodeString(h[1])
-
-    if err != nil {
-        c.RespondError(errors.New("Bad Request"), 400)
-
-        return
-    }
-
-    p := strings.Split(string(b), ":")
-
-    if p[0] != "bodokaiser" {
-        c.RespondError(errors.New("Unauthorized"), 401)
-
-        return
-    }
-
-    c.Next()
-}
-
-func logger(c *mux.Context) {
-    log.Printf("Request: %s %s", c.Method(), c.Path())
-
-    c.Next()
 }
---
+++ lib/httpd/mux/handles.go
@@ -0,0 +1,42 @@
+package mux
+
+import (
+    "log"
+    "errors"
+    "strings"
+    "encoding/base64"
+)
+
+func Auth(c *Context) {
+    h := strings.Split(c.Get("Authorization"), " ")
+
+    if len(h) != 2 {
+        c.RespondError(errors.New("Unauthorized"), 401)
+
+        return
+    }
+
+    b, err := base64.StdEncoding.DecodeString(h[1])
+
+    if err != nil {
+        c.RespondError(errors.New("Bad Request"), 400)
+
+        return
+    }
+
+    p := strings.Split(string(b), ":")
+
+    if p[0] != "bodokaiser" {
+        c.RespondError(errors.New("Unauthorized"), 401)
+
+        return
+    }
+
+    c.Next()
+}
+
+func Logger(c *Context) {
+    log.Printf("Request: %s %s", c.Method(), c.Path())
+
+    c.Next()
+}
