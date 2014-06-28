dc072c139201fd67a4a3250349275b78d091359f
Bodo Kaiser <i@bodokaiser.io>
2014-06-28 10:48:02 +0200

    fixed line indention

--- conf/conf.go
+++ conf/conf.go
@@ -1,27 +1,27 @@
 package conf
 
 import (
-    "flag"
+	"flag"
 )
 
 type Conf struct {
-    Httpd map[string]string
-    Store map[string]string
+	Httpd map[string]string
+	Store map[string]string
 }
 
 func New() *Conf {
-    return &Conf{
-        map[string]string{},
-        map[string]string{},
-    }
+	return &Conf{
+		map[string]string{},
+		map[string]string{},
+	}
 }
 
 func (c *Conf) FromFlags() error {
-    c.Httpd["addr"] = *flag.String("port", ":3001",
-        "Port to listen for incoming HTTP requests.")
+	c.Httpd["addr"] = *flag.String("port", ":3001",
+		"Port to listen for incoming HTTP requests.")
 
-    c.Store["mongo"] = *flag.String("mongo", "mongodb://localhost/satisfeet",
-        "URL to connect to mongodb server.")
+	c.Store["mongo"] = *flag.String("mongo", "mongodb://localhost/satisfeet",
+		"URL to connect to mongodb server.")
 
-    return nil
+	return nil
 }
--- httpd/router/handles.go
+++ httpd/router/handles.go
@@ -1,42 +1,42 @@
 package router
 
 import (
-    "log"
-    "errors"
-    "strings"
-    "encoding/base64"
+	"log"
+	"errors"
+	"strings"
+	"encoding/base64"
 )
 
 func Auth(c *Context) {
-    h := strings.Split(c.Get("Authorization"), " ")
+	h := strings.Split(c.Get("Authorization"), " ")
 
-    if len(h) != 2 {
-        c.RespondError(errors.New("Unauthorized"), 401)
+	if len(h) != 2 {
+		c.RespondError(errors.New("Unauthorized"), 401)
 
-        return
-    }
+		return
+	}
 
-    b, err := base64.StdEncoding.DecodeString(h[1])
+	b, err := base64.StdEncoding.DecodeString(h[1])
 
-    if err != nil {
-        c.RespondError(errors.New("Bad Request"), 400)
+	if err != nil {
+		c.RespondError(errors.New("Bad Request"), 400)
 
-        return
-    }
+		return
+	}
 
-    p := strings.Split(string(b), ":")
+	p := strings.Split(string(b), ":")
 
-    if p[0] != "bodokaiser" {
-        c.RespondError(errors.New("Unauthorized"), 401)
+	if p[0] != "bodokaiser" {
+		c.RespondError(errors.New("Unauthorized"), 401)
 
-        return
-    }
+		return
+	}
 
-    c.Next()
+	c.Next()
 }
 
 func Logger(c *Context) {
-    log.Printf("Request: %s %s", c.Method(), c.Path())
+	log.Printf("Request: %s %s", c.Method(), c.Path())
 
-    c.Next()
+	c.Next()
 }
