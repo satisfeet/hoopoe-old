714f10c6ea7d71550edef95b330a6bc39fc8f191
Bodo Kaiser <i@bodokaiser.io>
2014-06-30 17:40:15 +0200

    added Create, Update and Remove to store/customers

--- store/customers/customers.go
+++ store/customers/customers.go
@@ -9,19 +9,20 @@ var (
 	db *mgo.Database
 )
 
-type Customer struct {
-	Id      bson.ObjectId   `bson:"_id"     json:"id"`
-	Name    string          `bson:"name"    json:"name"`
-	Email   string          `bson:"email"   json:"email"`
-	Company string          `bson:"company" json:"company,omitempty"`
-	Address CustomerAddress `bson:"address" json:"address"`
-}
-
-type CustomerAddress struct {
-	Street string `bson:"street"  json:"street,omitempty"`
-	City   string `bson:"city"    json:"city,omitempty"`
-	Zip    uint16 `bson:"zip"     json:"zip,omitempty"`
-}
+type (
+	Customer struct {
+		Id      bson.ObjectId   `bson:"_id"     json:"id"`
+		Name    string          `bson:"name"    json:"name"`
+		Email   string          `bson:"email"   json:"email"`
+		Company string          `bson:"company" json:"company,omitempty"`
+		Address CustomerAddress `bson:"address" json:"address"`
+	}
+	CustomerAddress struct {
+		Street string `bson:"street"  json:"street,omitempty"`
+		City   string `bson:"city"    json:"city,omitempty"`
+		Zip    uint16 `bson:"zip"     json:"zip,omitempty"`
+	}
+)
 
 func Setup(session *mgo.Session) {
 	db = session.DB("")
@@ -40,6 +41,18 @@ func Setup(session *mgo.Session) {
 			"address.street",
 		},
 	})
+}
+
+func Create(customer *Customer) error {
+	return db.C("customers").Insert(customer)
+}
+
+func Update(customer *Customer) error {
+	return db.C("customer").UpdateId(customer.id, customer)
+}
+
+func Remove(customer *Customer) error {
+	return db.C("customers").RemoveId(customer.Id)
 }
 
 func FindAll(query *Query) ([]Customer, error) {
