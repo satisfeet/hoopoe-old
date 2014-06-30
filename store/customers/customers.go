4afe1cb72ea95cb675e1c3661cd9a0582e665b4b
Bodo Kaiser <i@bodokaiser.io>
2014-06-30 17:30:10 +0200

    moved store/customers/model into store/customers/customers

--- store/customers/customers.go
+++ store/customers/customers.go
@@ -2,11 +2,26 @@ package customers
 
 import (
 	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
 var (
 	db *mgo.Database
 )
+
+type Customer struct {
+	Id      bson.ObjectId   `bson:"_id"     json:"id"`
+	Name    string          `bson:"name"    json:"name"`
+	Email   string          `bson:"email"   json:"email"`
+	Company string          `bson:"company" json:"company,omitempty"`
+	Address CustomerAddress `bson:"address" json:"address"`
+}
+
+type CustomerAddress struct {
+	Street string `bson:"street"  json:"street,omitempty"`
+	City   string `bson:"city"    json:"city,omitempty"`
+	Zip    uint16 `bson:"zip"     json:"zip,omitempty"`
+}
 
 func Setup(session *mgo.Session) {
 	db = session.DB("")
