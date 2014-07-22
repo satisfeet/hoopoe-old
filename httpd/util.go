c1484a2b0d37f6744d304a1289d874a299f81892
Bodo Kaiser <i@bodokaiser.io>
2014-07-22 09:27:38 +0200

    moved httpd/handler into httpd/util

--- httpd/util.go
+++ httpd/util.go
@@ -1,12 +1,44 @@
 package httpd
 
 import (
+	"encoding/base64"
 	"net/http"
+	"strings"
 
 	"gopkg.in/mgo.v2"
 
+	"github.com/satisfeet/hoopoe/httpd/context"
 	"github.com/satisfeet/hoopoe/store"
 )
+
+var (
+	Basic = ""
+)
+
+func Auth(h http.Handler) http.Handler {
+	b := base64.StdEncoding.EncodeToString([]byte(Basic))
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c := context.NewContext(w, r)
+		a := c.Get("Authorization")
+
+		if i := strings.IndexRune(a, ' '); i != -1 {
+			if b == a[i+1:] {
+				h.ServeHTTP(w, r)
+				return
+			}
+		}
+
+		c.Set("WWW-Authenticate", "Basic realm=hoopoe")
+		c.Error(nil, http.StatusUnauthorized)
+	})
+}
+
+func NotFound() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		context.NewContext(w, r).Error(nil, http.StatusNotFound)
+	})
+}
 
 // ErrorCode retrieves the correct http error code
 // depending on the provided error type.
--- httpd/util_test.go
+++ httpd/util_test.go
@@ -3,6 +3,7 @@ package httpd
 import (
 	"errors"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"gopkg.in/check.v1"
@@ -20,6 +21,46 @@ func TestUtil(t *testing.T) {
 }
 
 type UtilSuite struct{}
+
+func (s *UtilSuite) TestAuth(c *check.C) {
+	req := make([]*http.Request, 3)
+	res := make([]*httptest.ResponseRecorder, 3)
+
+	for i := 0; i < 3; i++ {
+		req[i], _ = http.NewRequest("GET", "/", nil)
+		res[i] = httptest.NewRecorder()
+	}
+
+	req[0].SetBasicAuth("foo", "bar")
+	req[1].SetBasicAuth("foo", "bla")
+	req[2].Header.Add("Authorization", "Basic ")
+
+	Basic = "foo:bar"
+	Auth(s).ServeHTTP(res[0], req[0])
+	Auth(s).ServeHTTP(res[1], req[1])
+	Auth(s).ServeHTTP(res[2], req[2])
+
+	c.Check(res[0].Code, check.Equals, http.StatusOK)
+	c.Check(res[1].Code, check.Equals, http.StatusUnauthorized)
+	c.Check(res[2].Code, check.Equals, http.StatusUnauthorized)
+
+	c.Check(res[0].Body.String(), check.Equals, "Hello World")
+}
+
+func (s *UtilSuite) TestNotFound(c *check.C) {
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/bla", nil)
+
+	NotFound().ServeHTTP(res, req)
+
+	c.Check(res.Code, check.Equals, http.StatusNotFound)
+	c.Check(res.Body.String(), check.Equals, "{\"error\":\"Not Found\"}\n")
+}
+
+func (s *UtilSuite) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("Hello World"))
+}
 
 func (s *UtilSuite) TestErrorCode(c *check.C) {
 	c.Check(ErrorCode(ErrTest), check.Equals, http.StatusInternalServerError)
