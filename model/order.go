3ce92ec9a32031e9671a92592dd7556fe7c22fd4
Bodo Kaiser <i@bodokaiser.io>
2014-07-23 16:10:28 +0200

    added validation support to model/order

--- model/order.go
+++ model/order.go
@@ -1,28 +1,90 @@
 package model
 
 import (
-	"time"
+	"strconv"
 
+	"github.com/satisfeet/hoopoe/model/validation"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type Order struct {
 	Id       bson.ObjectId `json:"id"     bson:"_id"`
-	State    OrderState    `json:"state"`
 	Items    []OrderItem   `json:"items"`
+	Pricing  Pricing       `json:"pricing"`
 	Customer mgo.DBRef     `json:"customer"`
 }
 
-type OrderState struct {
-	Created time.Time `json:"created"`
-	Shipped time.Time `json:"shipped"`
-	Cleared time.Time `json:"cleared"`
+func (o Order) Validate() error {
+	errs := validation.Error{}
+
+	if err := validation.Required(o.Customer.Id); err != nil {
+		errs.Set("customer", err)
+	}
+	if err := validation.Required(o.Items); err != nil {
+		errs.Set("items", err)
+	}
+	if err := o.Pricing.Validate(); err != nil {
+		errs.Set("pricing", err)
+	}
+
+	for n, i := range o.Items {
+		if err := i.Validate(); err != nil {
+			errs.Set("item #"+strconv.Itoa(n), err)
+
+			break
+		}
+	}
+
+	if errs.Has() {
+		return errs
+	}
+	return nil
+}
+
+func (o Order) SetCustomer(c Customer) {
+	o.Customer = mgo.DBRef{
+		Id:         c.Id,
+		Collection: "customers",
+	}
 }
 
 type OrderItem struct {
 	Product   mgo.DBRef `json:"product"`
-	Pricing   Pricing   `json:"price"`
 	Quantity  int       `json:"quantity"`
+	Pricing   Pricing   `json:"price"`
 	Variation Variation `json:"variation"`
+}
+
+func (i OrderItem) Validate() error {
+	errs := validation.Error{}
+
+	if err := validation.Required(i.Product.Id); err != nil {
+		errs.Set("product", err)
+	}
+	if err := validation.Required(i.Quantity); err == nil {
+		if err := validation.Range(i.Quantity, 1, 0); err != nil {
+			errs.Set("quantity", err)
+		}
+	} else {
+		errs.Set("quantity", err)
+	}
+	if err := i.Pricing.Validate(); err != nil {
+		errs.Set("pricing", err)
+	}
+	if err := i.Variation.Validate(); err != nil {
+		errs.Set("variation", err)
+	}
+
+	if errs.Has() {
+		return errs
+	}
+	return nil
+}
+
+func (i OrderItem) SetProduct(p Product) {
+	i.Product = mgo.DBRef{
+		Id:         p.Id,
+		Collection: "products",
+	}
 }
--- model/order_test.go
+++ model/order_test.go
@@ -1,1 +1,101 @@
 package model
+
+import (
+	"testing"
+
+	"gopkg.in/check.v1"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+func TestOrder(t *testing.T) {
+	check.Suite(&OrderSuite{})
+	check.TestingT(t)
+}
+
+func TestOrderItem(t *testing.T) {
+	check.Suite(&OrderSuite{})
+	check.TestingT(t)
+}
+
+type OrderSuite struct{}
+
+type OrderItemSuite struct{}
+
+func (s *OrderSuite) TestValidate(c *check.C) {
+	i := []OrderItem{
+		OrderItem{
+			Product: mgo.DBRef{
+				Id: bson.NewObjectId(),
+			},
+			Pricing: Pricing{
+				Retail: 299,
+			},
+			Quantity: 1,
+			Variation: Variation{
+				Size:  "38",
+				Color: "blue",
+			},
+		},
+	}
+
+	c.Check(Order{
+		Customer: mgo.DBRef{
+			Id: bson.NewObjectId(),
+		},
+		Items: i,
+		Pricing: Pricing{
+			Retail: 299,
+		},
+	}.Validate(), check.IsNil)
+}
+
+func (s *OrderItemSuite) TestValidate(c *check.C) {
+	p := Pricing{
+		Retail: 299,
+	}
+	v := Variation{
+		Color: "black",
+		Size:  "38",
+	}
+
+	c.Check(OrderItem{
+		Product: mgo.DBRef{
+			Id: bson.NewObjectId(),
+		},
+		Quantity:  1,
+		Pricing:   p,
+		Variation: v,
+	}.Validate(), check.IsNil)
+
+	c.Check(OrderItem{
+		Quantity:  1,
+		Pricing:   p,
+		Variation: v,
+	}.Validate(), check.ErrorMatches, "product has invalid .*")
+
+	c.Check(OrderItem{
+		Product: mgo.DBRef{
+			Id: bson.NewObjectId(),
+		},
+		Quantity:  1,
+		Variation: v,
+	}.Validate(), check.ErrorMatches, "pricing has invalid .*")
+
+	c.Check(OrderItem{
+		Product: mgo.DBRef{
+			Id: bson.NewObjectId(),
+		},
+		Quantity:  0,
+		Pricing:   p,
+		Variation: v,
+	}.Validate(), check.ErrorMatches, "quantity has invalid .*")
+
+	c.Check(OrderItem{
+		Product: mgo.DBRef{
+			Id: bson.NewObjectId(),
+		},
+		Quantity: 1,
+		Pricing:  p,
+	}.Validate(), check.ErrorMatches, "variation has invalid .*")
+}
