d2122b4c731bae04e2ff42d5b31d8fef526a09ed
Bodo Kaiser <i@bodokaiser.io>
2014-07-24 12:42:34 +0200

    added optional image field to product model
    The image field stores small images as base64 encoded string which
    should be fine for the images we have. However there may need to be
    an adjustment made when we store multiple images.

--- model/product.go
+++ model/product.go
@@ -9,6 +9,7 @@ import (
 type Product struct {
 	Id          bson.ObjectId `json:"id" bson:"_id"`
 	Name        string        `json:"name" validate:"required,min=10,max=20"`
+	Images      []string      `json:"image" validate:"required,min=1"`
 	Pricing     Pricing       `json:"pricing" validate:"required,nested"`
 	Variations  []Variation   `json:"variations" validate:"required,nested"`
 	Description string        `json:"description" validate:"required,min=60"`
--- model/product_test.go
+++ model/product_test.go
@@ -14,6 +14,9 @@ func TestProduct(t *testing.T) {
 type ProductSuite struct{}
 
 func (s *ProductSuite) TestValidate(c *check.C) {
+	i := []string{
+		"24781374dhsfhkhk",
+	}
 	p := Pricing{
 		Retail: 299,
 	}
@@ -26,6 +29,7 @@ func (s *ProductSuite) TestValidate(c *check.C) {
 
 	c.Check(Product{
 		Name:       "Business Socks",
+		Images:     i,
 		Pricing:    p,
 		Variations: v,
 		Description: `
@@ -36,6 +40,7 @@ func (s *ProductSuite) TestValidate(c *check.C) {
 	}.Validate(), check.IsNil)
 
 	c.Check(Product{
+		Images:     i,
 		Pricing:    p,
 		Variations: v,
 		Description: `
@@ -47,6 +52,18 @@ func (s *ProductSuite) TestValidate(c *check.C) {
 
 	c.Check(Product{
 		Name:       "Business Socks",
+		Pricing:    p,
+		Variations: v,
+		Description: `
+			These are some Business socks!
+			They are really really nice.
+			You should try them!
+		`,
+	}.Validate(), check.ErrorMatches, "Images.*")
+
+	c.Check(Product{
+		Name:       "Business Socks",
+		Images:     i,
 		Variations: v,
 		Description: `
 			These are some Business socks!
@@ -57,6 +74,7 @@ func (s *ProductSuite) TestValidate(c *check.C) {
 
 	c.Check(Product{
 		Name:    "Business Socks",
+		Images:  i,
 		Pricing: p,
 		Description: `
 			These are some Business socks!
@@ -67,6 +85,7 @@ func (s *ProductSuite) TestValidate(c *check.C) {
 
 	c.Check(Product{
 		Name:       "Business Socks",
+		Images:     i,
 		Pricing:    p,
 		Variations: v,
 	}.Validate(), check.ErrorMatches, "Description.*")
